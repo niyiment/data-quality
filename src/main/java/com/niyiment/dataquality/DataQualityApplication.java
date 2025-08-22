06d9b3a136da70dbe693b61ca6c0e9c2cad26067
Niyi Ogungbemi <niyiment@gmail.com>
2025-08-22 12:21:58 +0100

    first commit

---
+++ src/main/java/com/niyiment/dataquality/DataQualityApplication.java
@@ -0,0 +1,13 @@
+package com.niyiment.dataquality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataQualityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niyiment/dataquality/DataqualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niyiment.dataquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataqualityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
